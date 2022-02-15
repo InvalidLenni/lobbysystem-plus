eac69292e045874eb0cc78dd9210ee03cb8020b0
InvalidLenni <68335355+InvalidLenni@users.noreply.github.com>
2022-02-15 16:45:54 +0100

    Add files via upload

---
+++ eventsystem/src/main/java/de/lenni/lobbysystem/commands/DiscordCommand.java
@@ -0,0 +1,31 @@
+package de.lenni.lobbysystem.commands;
+
+import de.lenni.lobbysystem.LobbySystem;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class DiscordCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(!(sender instanceof Player)) {
+            LobbySystem.INSTANCE.log("§7You aren't a player!");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        if(player.hasPermission("de.lenni.lobbysystem.commands.discord")) {
+            player.setHealth(20d);
+            player.setFoodLevel(20);
+            player.sendMessage(LobbySystem.PREFIX + "§b&lDiscord&: &7YourDiscordInvite");
+        } else {
+            player.sendMessage(LobbySystem.PREFIX + "§aYou don't have permissions for this!");
+        }
+        return true;
+    }
+}
